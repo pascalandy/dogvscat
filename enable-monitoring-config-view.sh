61df1b03a02839d7c46fffba0ee2bb29147868e5
Pascal Andy <pascal@firepress.org>
2018-09-21 06:03:25 -0400

    rm / file that should not be directly in master
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- enable-monitoring-config-view.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-for server in {1..3}; do
-  echo && echo "Once rebooted, visually confirm the config has been apply on dvc${server}" && \
-  docker-machine ssh dvc${server} cat /etc/docker/daemon.json;
-done;
